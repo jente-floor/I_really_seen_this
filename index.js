c5388e6bacc8ddf052be60e849abb90d7b15f50a
Arjen <arjen@pop-os.localdomain>
2020-06-08 14:28:00 +0200

    images loaded

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const setImages = () => {
-    let images = document.getElementById("images")
-    let image = document.createElement("img")
-    image.src = "images/pagina1/mountain.png"
-    images.appendChild(image)
+const showImages = () => {
+
 }
 
-setImages()
+showImages()
