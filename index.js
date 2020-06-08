148d817d24a4f77e9d660034bd5d34b8e6e13e76
Arjen <arjen@pop-os.localdomain>
2020-06-08 15:12:33 +0200

    images

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const showImages = () => {
+function showImages() {
+    setTimeout(() => unhideImage("one"), 3000)
+    setTimeout(() => unhideImage("two"), 6000)
+    setTimeout(() => unhideImage("three"), 9000)
+    setTimeout(() => unhideImage("four"), 12000)
+    setTimeout(() => unhideImage("five"), 15000)
+}
 
+
+function unhideImage(theimage) {
+    var element = document.getElementById(theimage);
+    element.classList.remove("hideItem");
 }
 
 showImages()
